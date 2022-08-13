616f138fbfd9243d37cd036c85546fbc0c81d9c7
Your Name <you@example.com>
2022-08-13 19:51:52 +0530

    create new rg2

--- rg.tf
+++ rg.tf
@@ -4,3 +4,9 @@ resource "azurerm_resource_group" "rg_name" {
   name     = "rg1"
   location = "eastus"
 }
+
+
+resource "azurerm_resource_group" "rg_name2" {
+  name     = "rg2"
+  location = "eastus"
+}
